3735136879d53f73b1dc838f9544e2636b816381
latam-developer <soporte_it@gpplatam.com>
2024-07-10 21:16:28 -0400

    changes +

--- Calculator.Console/Program.cs
+++ Calculator.Console/Program.cs
@@ -1,4 +1,84 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Data;
+using System.Net.Http.Headers;
+using Optional;
+using RPNCalculator;
+
+string menu =
+@"
+1. Solve a rpn operation.
+99. Exit
+";
+
+
+Console.WriteLine("------------------Reverse Polish Notatino calculator---------------");
+
+System.Console.WriteLine("\nSelect an option on the menu");
+
+
+bool exit = false;
+string input = "";
+
+string output = "";
+
+while (!exit)
+{
+    PrintMenu();
+
+    input = Console.ReadLine();
+
+    switch (input)
+    {
+        case "1":
+            DoOperation();
+            Console.ReadLine();
+            Console.Clear();
+            break;
+        case "99":
+            exit = true;
+            break;
+        default:
+            break;
+    }
+
+
+}
+
+void DoOperation()
+{
+
+    Calculator_rpn _calculator = new Calculator_rpn();
+
+    //Create input
+
+    System.Console.WriteLine("-->Write the RPN operation to solve: ");
+    string rpn_notation = Console.ReadLine();
+
+    Option<RpnNotation, string> rpn = RpnNotation.From(rpn_notation);
+
+    string result = _calculator.Calculate(rpn);
+
+    //print result
+    System.Console.WriteLine("\nYour result: " + result);
+
+}
+
+void PrintMenu()
+{
+
+    System.Console.WriteLine(menu);
+}
+
+public class Calculator_rpn
+{
+    public string Calculate(Option<RpnNotation, string> rpn)
+    {
+
+        RpnCalculation calculator = new RpnCalculation(rpn);
+        string result = calculator.ResultOrError();
+
+        return result;
+    }
+}
 
 
--- Calculator.Test/UnitTest1.cs
+++ Calculator.Test/UnitTest1.cs
@@ -1,3 +1,6 @@
+using Optional;
+using RPNCalculator;
+
 namespace Calculator.Test;
 
 public class Tests
@@ -7,9 +10,43 @@ public class Tests
     {
     }
 
-    [Test]
-    public void Test1()
+    [TestCase("20 5 /", "4")]
+    [TestCase("20 5 +", "25")]
+    [TestCase("20 5 -", "15")]
+    [TestCase("20 5 *", "100")]
+    [TestCase("100 5 /", "20")]
+    [TestCase("20 4 / ", "5")]
+    public void Calculator_solves_basic_Operations(string input, string expectResult)
     {
-        Assert.Pass();
+        Calculator_rpn _calculator = new Calculator_rpn();
+
+        Option<RpnNotation, string> rpn = RpnNotation.From(input);
+        string result = _calculator.Calculate(rpn);
+
+
+        Assert.That(result, Is.EqualTo(expectResult));
     }
+
+
+    // 20 5 /
+    // result o error
+
+    /*
+    rpnnotation rpn = new RPnnotation(string input);
+
+RpnCalculator calcultator = new RPncalcultor(rpn);
+    string result =  obj.ResultOrErro() 
+
+
+Calculator 
+
+    CalculateAnswer
+
+
+
+    */
+
+
+
+    // 
 }
